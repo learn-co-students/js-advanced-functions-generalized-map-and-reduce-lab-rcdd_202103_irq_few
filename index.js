160206ac408a5867aa6a314c3096dd7c67893018
Ahmed Mohammed <ahmed1831990@gmail.com>
2021-05-14 15:08:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Add your functions here
+
+function map(sourceArray, functions){
+  return sourceArray.map(functions);
+}
+
+function reduce(sourceArray, functions, startingPoint=0){
+  if(startingPoint=="0") return sourceArray.reduce(functions);
+  else return sourceArray.reduce(functions, startingPoint);
+}
