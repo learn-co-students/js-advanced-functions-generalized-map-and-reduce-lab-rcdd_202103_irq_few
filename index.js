7605fcb42e85ff3e5ab53388cd68e42436311dae
Kizhan Mohammed <kizhan.m.kareem@gmail.com>
2021-05-19 06:05:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your functions here
+
+const map = (array, callBackFunc)=>{
+  let newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    let element = array[i];
+    newArray.push(callBackFunc(element));
+  }
+  return newArray;
+};
+
+const reduce = (array, callBackFunc, starter)=>{
+  if (starter) {
+      let entire = starter;
+      for (let i = 0; i < array.length; i++) {
+        let element = array[i];
+        entire = callBackFunc(entire, element);
+      }
+      return entire;
+  }
+  else {
+      let total = array[0];
+      for (let i = 1; i < array.length; i++) {
+        let element = array[i];
+        total = callBackFunc(total, element);
+      }
+      return total;
+  }
+
+};
