6778e413821032e4121ed343652f11fc5ff39a80
Omer Sabah <omerrj99@gmail.com>
2021-05-31 14:55:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your functions here
+
+
+const map=(src, callback) =>{
+  let r = []
+for (let i = 0; i < src.length; i++) {
+    let theElement = src[i]
+    r.push(callback(theElement))
+  }
+
+  return r;
+}
+
+const reduce=(src, callback, starting)=>{
+  let r = (starting) ? starting : src[0]
+  let i = (starting) ? 0 : 1
+
+  for (; i < src.length; i++) {
+    r = callback(src[i], r)
+  }
+
+  return r;
+}
