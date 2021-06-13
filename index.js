58d4d734f9871997df76f3f8ed390e5c7d230d26
Yusra Ahmed <isra.00287446@gmail.com>
2021-06-13 21:11:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your functions here
+function map(array, func) {
+    let retArray = []
+    for (const el of array) {
+        retArray.push(func(el))
+    }
+    return retArray
+}
+
+function reduce(array, func, startingPoint) {
+
+
+    let agg
+    if (startingPoint) {
+        agg = startingPoint
+     }  
+
+    for (let i = 0; i < array.length; i++) {
+
+        if (typeof agg === "undefined") {
+            agg = func(array[1], array[0])
+            i++
+        } else {
+            agg = func(array[i], agg)
+        }
+
+    }
+
+    return agg
+} 
